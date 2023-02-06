5c865ed8a290445d358d1a1daaa7487e1cfe3412
Samuel Kim <samuelskim420@gmail.com>
2023-02-06 21:04:17 +0000

    fix count

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -29,10 +29,9 @@ class MaxIntSet
   end
 end
 
-
 class IntSet
 
-  # attr_reader :store
+  attr_reader :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -43,19 +42,31 @@ class IntSet
   end
 
   def remove(num)
+    self[num].pop(num)
   end
 
   def include?(num)
+
+    store.each do |bucket|
+      bucket.each do |el|
+        if el == num
+          return true
+        end
+      end
+    end
+
+    false
+
   end
+  
+  
+  private
   
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
     i = num % num_buckets
     return @store[i]
   end
-  
-  private
-  
 
   def num_buckets
     @store.length
@@ -63,7 +74,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -71,24 +82,52 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !store.include?(num)
+      if count = store.length
+        # self.resize!
+        self[num] << num
+        count += 1
+      else
+        self[num] << num
+        count += 1
+      end
+    end
+
   end
 
   def remove(num)
   end
 
+  def count
+    @count 
+  end
+
   def include?(num)
+    store.each do |bucket|
+      bucket.each do |el|
+        if el == num
+          return true
+        end
+      end
+    end
+
+    false
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
+
 
   def num_buckets
     @store.length
   end
 
   def resize!
+    @store = @store * 2
+  
   end
 end
