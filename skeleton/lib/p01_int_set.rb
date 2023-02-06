0c306a6d876c2f6bb87b01dd94ed58635abc9e10
andy <morrison.andrew422@gmail.com>
2023-02-06 14:50:09 -0500

    start IntSet

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -5,7 +5,7 @@ class MaxIntSet
   def initialize(max)
     @store = Array.new(max)
   end
-
+ 
   def insert(num)
     raise 'Out of bounds' if num >= @store.length || num < 0
     @store[num] = true
@@ -31,11 +31,15 @@ end
 
 
 class IntSet
+
+  # attr_reader :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
@@ -43,12 +47,15 @@ class IntSet
 
   def include?(num)
   end
-
-  private
-
+  
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    i = num % num_buckets
+    return @store[i]
   end
+  
+  private
+  
 
   def num_buckets
     @store.length
